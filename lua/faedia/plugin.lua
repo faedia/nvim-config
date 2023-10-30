0a0b3f2ae79d09324f828426bdaa74af4228c135
Steven Tomlinson <t97steven@gmail.com>
2023-10-30 16:47:03 +0000

    Add support for markdown preview

--- lua/faedia/plugin.lua
+++ lua/faedia/plugin.lua
@@ -95,4 +95,15 @@ require('lazy').setup({
 		},
 		build = ':TSUpdate',
 	},
+
+	-- add markdown preview support
+	{
+		'iamcco/markdown-preview.nvim',
+		cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
+		build = "cd app && yarn install",
+		init = function()
+			vim.g.mkdp_filetypes = {"markdown"}
+		end,
+		ft = { 'markdown' },
+	},
 }, {})
